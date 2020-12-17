3d8f2532fcabc0ce0332222cc79c8ea161b82a5a
fugamiyata <72694228+fugamiyata@users.noreply.github.com>
2020-12-17 15:06:38 +0900

    Initial commit

---
+++ Assets/ScoreScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    
+
+    public GameObject scoreText ;
+
+    public int score = 0;
+
+    void Start()
+    {
+        // シーン中のscoretextオブジェクトを表示
+        this.scoreText = GameObject.Find("scoreText");
+
+
+        score = 0;
+        SetScore();
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallStarTag")
+        {
+            score += 10;
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            score += 20;
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            score += 15;
+
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            score += 7;
+        }
+
+        SetScore();
+    }
+
+    void SetScore()
+    {
+        this.scoreText.GetComponent<Text>().text = "得点：" + score;
+    }
+
+}
